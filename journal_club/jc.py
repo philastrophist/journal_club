b4606651eefb6e43eb631a8c8e9b5973454d7333
Shaun C Read <shaun.c.read@gmail.com>
2017-11-23 21:03:05 +0000

    cleanup

--- journal_club/jc.py
+++ journal_club/jc.py
@@ -123,8 +123,6 @@ def choose(args):
 
     all_names = set(record.index.values.tolist() + attend + missing)
     missing = (set(all_names) & set(missing)) - set(attend)
-    # new_missing = set(missing) - set(all_names)
-    # old_missing = set(all_missing) - set(new_missing)
     record = update(record.reindex(all_names), verbose=args.verbose)
 
     record.loc[missing, 'misses'] += 1
